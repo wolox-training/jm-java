f659e01d4599bbee6b875b724d3605eb25b03b6d
Jhon Munoz <jhonathan.munoz@wolox.cl>
2020-04-21 15:29:32 -0400

    removes unused annotation

--- src/main/java/com/wolox/training/repositories/BookRepository.java
+++ src/main/java/com/wolox/training/repositories/BookRepository.java
@@ -2,12 +2,10 @@ package com.wolox.training.repositories;
 
 import com.wolox.training.models.Book;
 import java.util.Optional;
-import org.hibernate.validator.constraints.UniqueElements;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
 
-    @UniqueElements
     Optional<Book> findTopByAuthor(final String author);
 
 }
