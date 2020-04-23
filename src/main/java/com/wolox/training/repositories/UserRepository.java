f31adbd1f5bed8571367b531a59049c9833b2328
Jhon Munoz <jhonathan.munoz@wolox.cl>
2020-04-23 16:57:13 -0400

    implements user repository

---
+++ src/main/java/com/wolox/training/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.wolox.training.repositories;
+
+import com.wolox.training.models.User;
+import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    Optional<User> findTopByName(final String name);
+
+}
