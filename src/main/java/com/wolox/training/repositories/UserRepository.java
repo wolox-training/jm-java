0d628119f34c5cdb3897a628a0bf821aa924d644
Jhon Munoz <jhonathan.munoz@wolox.cl>
2020-04-23 16:50:42 -0400

    deletes unused repo

--- src/main/java/com/wolox/training/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wolox.training.repositories;
-
-import com.wolox.training.models.Book;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<Book, Long> {
-
-
-}
