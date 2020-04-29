c65e15e7cdb18fc2a8244d933138b1921e5b739f
Jhon Munoz <jhonathan.munoz@wolox.cl>
2020-04-29 16:30:14 -0400

    implements test for Users

--- src/test/java/com/wolox/training/models/UserTest.java
+++ src/test/java/com/wolox/training/models/UserTest.java
@@ -1,25 +1,53 @@
 package com.wolox.training.models;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.BeforeEach;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import com.wolox.training.repositories.UserRepository;
+import java.time.LocalDate;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@DataJpaTest
 class UserTest {
 
-    @BeforeEach
-    void setUp() {
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Test
+    public void whenFindTopByName_thenReturnUser() {
+        User user = new User("jhon_doe", "Jhon Doe", LocalDate.of(1990,1,1));
+        entityManager.persist(user);
+        entityManager.flush();
+        Optional<User> userFound = userRepository.findTopByName(user.getName());
+        assertThat(userFound.get()).usingRecursiveComparison().isEqualTo(user);
     }
 
     @Test
-    void getId() {
+    public void whenNoName_throwsException() {
+        User user = new User("jhon_doe", "Jhon Doe", LocalDate.of(1990,1,1));
+        assertThrows(NullPointerException.class, () -> user.setName(null));
     }
 
     @Test
-    void getUsername() {
+    public void whenNoUsername_throwsException() {
+        User user = new User("jhon_doe", "Jhon Doe", LocalDate.of(1990,1,1));
+        assertThrows(NullPointerException.class, () -> user.setUsername(null));
     }
 
     @Test
-    void setUsername() {
+    public void whenNoBirthDate_throwsException() {
+        User user = new User("jhon_doe", "Jhon Doe", LocalDate.of(1990,1,1));
+        assertThrows(NullPointerException.class, () -> user.setBirthDate(null));
     }
+
 }
